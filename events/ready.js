65ec70e136df69dceb8744c45554fa1fd7b29093
Horse1jj <foste1jj.seth@gmail.com>
2025-02-20 16:52:29 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,36 +1,36 @@
-
 const Event = require('../structures/Event');
 const config = require('../config.json');
 
 module.exports = class extends Event {
-    async run() {
-
-    // ready interval 
-
-    
+  async run() {
     const activities = [
-      { name: `${config.bot_name || 'Ticket bot'}`, type: 'WATCHING' }, 
-      { name: 'github.com/peterhanania/ticket-bot', type: 'WATCHING' }
+      { name: `${config.bot_name || 'Ticket bot'}`, type: 3 }, // WATCHING = 3
+      { name: 'Pavlov Prison Life', type: 3 },
     ];
-  
 
-    this.client.user.setPresence({ status: 'online', activity: activities[0] });
-  
+    this.client.user.setPresence({
+      status: 'online',
+      activities: [activities[0]],
+    });
+
     let activity = 1;
-  
 
     setInterval(() => {
-      activities[2] = { name: `${config.prefix}help | ${ this.client.guilds.cache.size} guilds`, type: 'WATCHING' };
-      activities[3] = { name: `${config.prefix}help | ${ this.client.users.cache.size} users`, type: 'WATCHING' }; 
-      if (activity > 3) activity = 0;
-      this.client.user.setActivity(activities[activity]);
+      const dynamicActivities = [
+        { name: `${config.prefix}help | ${this.client.guilds.cache.size} guilds`, type: 3 },
+        { name: `${config.prefix}help | ${this.client.users.cache.size} users`, type: 3 },
+      ];
+
+      const allActivities = [...activities, ...dynamicActivities];
+
+      if (activity >= allActivities.length) activity = 0;
+
+      this.client.user.setActivity({
+        name: allActivities[activity].name,
+        type: allActivities[activity].type,
+      });
+
       activity++;
     }, 35000);
-
-
-      
-  
-}
-
-}
-
+  }
+};
