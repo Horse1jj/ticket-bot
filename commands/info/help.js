4578e4ff0bfa52cacffe94eee4ab1b99cf727b3a
Horse1jj <foste1jj.seth@gmail.com>
2025-02-20 16:44:31 -0500

    Fixed to djs 14

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,61 +1,92 @@
 const Command = require('../../structures/Command');
-const { MessageEmbed, DiscordAPIError } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const config = require('../../config.json');
 
 module.exports = class extends Command {
-    constructor(...args) {
-      super(...args, {
-        name: 'help',
-        aliases: ["help", "menu"],
-        description: `Display\'s ${config.bot_name || 'Bot'}\'s Help Menu.`,
-        category: 'Information',
-        cooldown: 3,
-      });
-    }
+  constructor(...args) {
+    super(...args, {
+      name: 'help',
+      aliases: ['help', 'menu'],
+      description: `Displays ${config.bot_name || 'Bot'}'s Help Menu.`,
+      category: 'Information',
+      cooldown: 3,
+    });
+  }
 
-    async run(message, args) {
-    
-    if(!args[0]) {
-    
-    const embed = new MessageEmbed()
-    .setColor('GREEN')
-    .setTitle(`${config.bot_name || 'Bot'}'s Command List`)
-    .setDescription(`**Please make sure to follow the rules`)
+  async run(message, args) {
+    if (!args[0]) {
+      const embed = new EmbedBuilder()
+        .setColor('Green')
+        .setTitle(`${config.bot_name || 'Bot'}'s Command List`)
+        .setDescription('**Please make sure to follow the rules**');
 
+      const categories = message.client.utils.removeDuplicates(
+        message.client.commands
+          .filter((cmd) => cmd.category !== 'Owner')
+          .map((cmd) => cmd.category)
+      );
 
-    const categories = message.client.utils.removeDuplicates(message.client.commands.filter(cmd => cmd.category !== 'Owner').map(cmd => cmd.category));
+      for (const category of categories) {
+        embed.addFields({
+          name: `**${category}**`,
+          value: message.client.commands
+            .filter((cmd) => cmd.category === category)
+            .map(
+              (cmd) =>
+                `\`${cmd.name}${' '.repeat(
+                  12 - Number(cmd.name.length)
+                )}:\` - ${cmd.description}`
+            )
+            .join('\n'),
+        });
+      }
 
-
-    for (const category of categories) {
-      embed.addField(`**${category}**`, this.client.commands.filter(cmd => 
-        cmd.category === category).map(cmd => `\`${cmd.name}${" ".repeat(12 - Number(cmd.name.length))}:\` - ${cmd.description}`).join('\n'))
-    }
-
-    message.channel.send(embed)
-    
+      message.channel.send({ embeds: [embed] });
     } else {
-        const cmd = this.client.commands.get(args[0]) || this.client.commands.get(this.client.aliases.get(args[0]));
-        if(!cmd) return message.channel.send(`Could not find the following command.`);
+      const cmd =
+        this.client.commands.get(args[0]) ||
+        this.client.commands.get(this.client.aliases.get(args[0]));
+      if (!cmd) return message.channel.send(`Could not find the following command.`);
 
-        const embed = new MessageEmbed()
-        embed.setTitle(`Command: ${cmd.name}`)
-        embed.setDescription(cmd.description)
-        embed.setFooter(cmd.disabled ? 'This command is currently disabled.' : message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
-       
-        
-        embed.addField('Usage',  `\`${cmd.usage}\``, true)
-        embed.addField('category',  `\`${capitalize(cmd.category)}\``, true)
+      const embed = new EmbedBuilder()
+        .setTitle(`Command: ${cmd.name}`)
+        .setDescription(cmd.description)
+        .setFooter({
+          text: cmd.disabled
+            ? 'This command is currently disabled.'
+            : message.member.displayName,
+          iconURL: message.author.displayAvatarURL({ dynamic: true }),
+        });
 
-        if(cmd.aliases && cmd.aliases.length) embed.addField('Aliases', cmd.aliases.map(alias => `\`${alias}\``, true).join(', '), true)
-        if(cmd.cooldown && cmd.cooldown > 1) embed.addField('Cooldown', `\`${cmd.cooldown}s\``, true)
-        if(cmd.examples && cmd.examples.length) embed.addField('__**Examples**__', cmd.examples.map(example => `- \`${example}\``).join('\n'))
+      embed.addFields(
+        { name: 'Usage', value: `\`${cmd.usage || 'No usage specified'}\``, inline: true },
+        { name: 'Category', value: `\`${capitalize(cmd.category)}\``, inline: true }
+      );
 
-        message.channel.send(embed)
-  
+      if (cmd.aliases && cmd.aliases.length) {
+        embed.addFields({
+          name: 'Aliases',
+          value: cmd.aliases.map((alias) => `\`${alias}\``).join(', '),
+          inline: true,
+        });
+      }
 
+      if (cmd.cooldown && cmd.cooldown > 1) {
+        embed.addFields({
+          name: 'Cooldown',
+          value: `\`${cmd.cooldown}s\``,
+          inline: true,
+        });
+      }
+
+      if (cmd.examples && cmd.examples.length) {
+        embed.addFields({
+          name: '__**Examples**__',
+          value: cmd.examples.map((example) => `- \`${example}\``).join('\n'),
+        });
+      }
+
+      message.channel.send({ embeds: [embed] });
     }
-    
-
-    }
+  }
 };
-
