a953d64de81d32215bbcd09afdcca2b06c90e133
Horse1jj <foste1jj.seth@gmail.com>
2025-02-20 16:48:37 -0500

    Update Event.js

--- structures/Event.js
+++ structures/Event.js
@@ -1,14 +1,23 @@
+const { Partials } = require('discord.js');
+
 module.exports = class Event {
   constructor(client, name, options = {}) {
     this.name = name;
-    this.partials = ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER', 'GUILD_MESSAGE_REACTIONS'],
+    this.partials = [
+      Partials.Message,
+      Partials.Channel,
+      Partials.Reaction,
+      Partials.GuildMember,
+      Partials.User,
+      Partials.GuildScheduledEvent
+    ];
     this.client = client;
     this.type = options.once ? 'once' : 'on';
-    this.emitter = (typeof options.emitter === 'string' ? this.client[options.emitter] : options.emitter) || this.client;
+    this.emitter =
+      (typeof options.emitter === 'string' ? this.client[options.emitter] : options.emitter) || this.client;
   }
 
-  // eslint-disable-next-line no-unused-vars
-  async run(message, args) {
+  async run(...args) {
     throw new Error(`The run method has not been implemented in ${this.name}`);
   }
 
