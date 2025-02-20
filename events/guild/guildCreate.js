5bc50c59e8fc6918805dae7dc87831b65853f1b1
Horse1jj <foste1jj.seth@gmail.com>
2025-02-20 17:19:56 -0500

    Update guildCreate.js

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -1,16 +1,16 @@
 const Event = require('../../structures/Event');
 const Discord = require('discord.js');
 
-
 module.exports = class extends Event {
 
   async run(guild) {
-  
-    console.log(`--| I just joined ${guild.name} |--`)
+    try {
+      console.log(`--| I just joined ${guild.name} |--`);
+      console.log(`Guild ID: ${guild.id}`);
+      console.log(`Total Members: ${guild.memberCount}`);
+      console.log(`Owner: ${guild.owner.user.tag}`);
 
-
-    
-
-    
-  }
-};
+      // Attempt to send a welcome message to the default channel
+      const defaultChannel = guild.systemChannel || guild.channels.cache.find(ch => ch.type === 'text');
+      
+     
