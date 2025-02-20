8c23925ebb10f9fe72d364928e9fbdddb592b728
Horse1jj <foste1jj.seth@gmail.com>
2025-02-20 16:47:43 -0500

    Updated to djs 14 and made better

--- structures/Command.js
+++ structures/Command.js
@@ -1,25 +1,23 @@
-const { Permissions } = require('discord.js');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = class Command {
     constructor(client, name, options = {}) {
         this.client = client;
         this.name = options.name || name;
         this.aliases = options.aliases || [];
-        this.description = options.description || "No description provided.";
-        this.category = options.category || "General";
-        this.usage = `${this.name} ${options.usage || ''}` || "No usage provided.";
+        this.description = options.description || 'No description provided.';
+        this.category = options.category || 'General';
+        this.usage = `${this.name} ${options.usage || ''}`.trim() || 'No usage provided.';
         this.examples = options.examples || [];
         this.disabled = options.disabled || false;
-        this.cooldown = "cooldown" in options ? options.cooldown : 5 || 5;
+        this.cooldown = 'cooldown' in options ? options.cooldown : 5;
         this.ownerOnly = options.ownerOnly || false;
         this.guildOnly = options.guildOnly || false;
         this.nsfwOnly = options.nsfwOnly || false;
-        this.botPermission = options.botPermission || ['SEND_MESSAGES', 'EMBED_LINKS'];
-        this.userPermission = options.userPermission  || null;
-        
+        this.botPermission = options.botPermission || [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks];
+        this.userPermission = options.userPermission || null;
     }
 
-    // eslint-disable-next-line no-unused-vars
     async run(message, args) {
         throw new Error(`The run method has not been implemented in ${this.name}`);
     }
